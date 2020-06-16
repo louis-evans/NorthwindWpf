81b1869e04908b1701f0eb1c0de7d4f2d5ef477d
levans <levans@LT5592>
2020-06-16 16:38:04 +0100

    added post code lookup service

---
+++ NorthwindWpf.Data/Models/AddressFindResult.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace NorthwindWpf.Data.Models
+{
+    public class AddressFindResult
+    {
+        [JsonProperty("postcode")]
+        public string PostCode { get; set; }
+
+        [JsonProperty("latitude")]
+        public double Latitude { get; set; }
+
+        [JsonProperty("longitude")]
+        public double Longitude { get; set; }
+
+        [JsonProperty("addresses")]
+        public IEnumerable<Address> Addresses { get; set; }
+
+        public class Address
+        {
+            [JsonProperty("formatted_address")]
+            public IEnumerable<string> FormattedAddress { get; set; }
+
+            [JsonProperty("thoroughfare")]
+            public string Thoroughfare { get; set; }
+
+            [JsonProperty("building_name")]
+            public string BuildingName { get; set; }
+
+            [JsonProperty("building_number")]
+            public string BuildingNumber { get; set; }
+
+            [JsonProperty("sub_building_name")]
+            public string SubBuildingName { get; set; }
+
+            [JsonProperty("sub_building_number")]
+            public string SubBuildingNumber { get; set; }
+
+            [JsonProperty("line_1")]
+            public string Line1 { get; set; }
+
+            [JsonProperty("line_2")]
+            public string Line2 { get; set; }
+
+            [JsonProperty("line_3")]
+            public string Line3 { get; set; }
+
+            [JsonProperty("line_4")]
+            public string Line4 { get; set; }
+
+            [JsonProperty("locality")]
+            public string Locality { get; set; }
+
+            [JsonProperty("town_or_city")]
+            public string TownCity { get; set; }
+
+            [JsonProperty("county")]
+            public string County { get; set; }
+
+            [JsonProperty("district")]
+            public string District { get; set; }
+
+            [JsonProperty("country")]
+            public string Country { get; set; }
+        }
+    }
+
+}
---
+++ NorthwindWpf.Data/Services/AddressLookupService.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using NorthwindWpf.Data.Models;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NorthwindWpf.Data.Services
+{
+    public class AddressLookupService : IDisposable
+    {
+        private readonly string _apiKey;
+        private readonly HttpClient _httpClient;
+
+        public AddressLookupService()
+        {
+            _apiKey = ConfigurationManager.AppSettings["AddressApiKey"];
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<AddressFindResult> FindByPostCodeAsync(string postCode, int? number = null)
+        {
+            string jsonText;
+
+#if DEBUG
+            var fileName = number == null ? "find_address_by_number" : "find_address_by_postcode";
+
+            var filePath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "Static", $"{fileName}_sample.json");
+            jsonText = File.ReadAllText(filePath);
+            
+#else
+            var apiUrl = ConfigurationManager.AppSettings["AddressFindUrl"];
+            var urlEnd = postCode + (number == null ? "" : $"/{number}");
+
+            apiUrl = string.Format(apiUrl, urlEnd, _apiKey);
+
+            var response = await _httpClient.GetAsync(apiUrl);
+            jsonText = await response.Content.ReadAsStringAsync();
+#endif
+
+            return JsonConvert.DeserializeObject<AddressFindResult>(jsonText);
+        }
+
+        public void Dispose()
+        {
+            _httpClient.Dispose();
+        }
+    }
+}
+
