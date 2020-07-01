9a9d2456feb8901dff74c5556a14ed0c81123a39
levans <levans@LT5592>
2020-07-01 13:20:14 +0100

    added generic way to open windows

--- WpfApp1/Views/MainWindow.xaml.cs
+++ WpfApp1/Views/MainWindow.xaml.cs
@@ -1,28 +1,52 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 using WpfApp1.Views;
 
 namespace NorthwindWpf.Views
 {
     public partial class MainWindow : Window
     {
+        private readonly ICollection<Window> _openWindows;
+
         public MainWindow()
         {
             InitializeComponent();
+            _openWindows = new List<Window>();
         }
 
         private void BtnViewOrders_Click(object sender, RoutedEventArgs e)
         {
-            new ViewOrdersWindow().ShowDialog();
+            OpenWindow<ViewOrdersWindow>(singleWindow:true);
         }
 
         private void BtnNewOrder_Click(object sender, RoutedEventArgs e)
         {
-            new OrderEntryWindow().ShowDialog();
+            OpenWindow<OrderEntryWindow>();
         }
 
         private void BtnProducts_Click(object sender, RoutedEventArgs e)
         {
+            OpenWindow<ViewProductsWindow>(singleWindow: true);
+        }
 
+        private void OpenWindow<T>(bool singleWindow = false) where T : Window, new()
+        {
+            if (singleWindow)
+            {
+                if (_openWindows.Any(w => w.GetType() == typeof(T)))
+                {
+                    _openWindows.First(w => w.GetType() == typeof(T)).Focus();
+                    return;
+                }
+            }
+            
+            var window = new T();
+            window.Closed += (sender, e) => _openWindows.Remove((Window)sender);
+
+            _openWindows.Add(window);
+
+            window.Show();
         }
     }
 }
