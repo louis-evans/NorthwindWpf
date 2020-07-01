40d31db22f4518974d2f01d60e9a8195cb851504
levans <levans@LT5592>
2020-07-01 10:07:54 +0100

    added view model and can now search with return key

---
+++ WpfApp1/ViewModels/AddressSearchViewModel.cs
@@ -0,0 +1,16 @@
+﻿using NorthwindWpf.Data.Models;
+using System.Collections.Generic;
+
+namespace WpfApp1.ViewModels
+{
+    public class AddressSearchViewModel : ViewModelBase
+    {
+        private string _postCode;
+        private string _nameNumber;
+        private IEnumerable<AddressFindResult.Address> _addresses;
+
+        public string PostCode { get => _postCode; set => SetProperty(ref _postCode, value); }
+        public string NameNumber { get => _nameNumber; set => SetProperty(ref _nameNumber, value); }
+        public IEnumerable<AddressFindResult.Address> Addresses { get => _addresses; set => SetProperty(ref _addresses, value); }
+    }
+}
--- WpfApp1/Views/AddressSearchWindow.xaml.cs
+++ WpfApp1/Views/AddressSearchWindow.xaml.cs
@@ -2,8 +2,13 @@
 using NorthwindWpf.Core.Services;
 using NorthwindWpf.Data.Models;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using WpfApp1.ViewModels;
 
 namespace WpfApp1.Views
 {
@@ -12,12 +17,20 @@ namespace WpfApp1.Views
         private readonly IAddressLookupService _addressService;
 
         public AddressFindResult.Address SelectedAddress { get; private set; }
+        private readonly AddressSearchViewModel _viewModel;
 
         public AddressSearchWindow()
         {
             InitializeComponent();
 
             _addressService = ServiceResolver.Get().Resolve<IAddressLookupService>();
+
+            _viewModel = new AddressSearchViewModel
+            {
+                Addresses = new List<AddressFindResult.Address>()
+            };
+
+            DataContext = _viewModel;
         }
 
         protected override void OnClosed(EventArgs e)
@@ -26,37 +39,60 @@ namespace WpfApp1.Views
             _addressService.Dispose();
         }
 
-        private async void Search_Click(object sender, RoutedEventArgs e)
+        //TODO 
+        // Implement a ICommand in the view model to handle the search event for both button and enter key
+
+        private void Search_Click(object sender, RoutedEventArgs e)
         {
-            var postCode = TxtPostCode.Text;
-            var number = 0;
-
-            if (string.IsNullOrWhiteSpace(postCode)) return; //TODO need an error message
-
-            if (!string.IsNullOrWhiteSpace(TxtNumber.Text))
-            {
-                if (!int.TryParse(TxtNumber.Text.Trim(), out number))
-                {
-                    //TODO need an error message
-                    return;
-                }
-            }
-
-            var addressResults = await _addressService.FindByPostCodeAsync(postCode, number == default ? (int?)null: number);
-            DgResults.ItemsSource = addressResults.Addresses;
+            RunSearch();
         }
 
-        private void Result_DoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void OnInputKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                RunSearch();
+            }
+            else
+            {
+                var txtSender = (TextBox)sender;
+
+                //do this because the view model value doent get updated until the field loses focus
+                if (txtSender == TxtPostCode) _viewModel.PostCode = txtSender.Text;
+                else if (txtSender == TxtNameNumber) _viewModel.NameNumber = txtSender.Text;
+            }
+        }
+
+        private void Result_DoubleClick(object sender, MouseButtonEventArgs e)
         {
             var row = (DataGridRow) sender;
             SelectedAddress = row.Item as AddressFindResult.Address;
 
             if (SelectedAddress != null)
             {
-                SelectedAddress.PostCode = TxtPostCode.Text.ToUpperInvariant();
+                SelectedAddress.PostCode = _viewModel.PostCode.ToUpperInvariant();
                 DialogResult = true;
                 Close();
             }
+        }
+
+        private async void RunSearch()
+        {
+            var number = 0;
+
+            if (string.IsNullOrWhiteSpace(_viewModel.PostCode)) return; //TODO need an error message
+
+            if (!string.IsNullOrWhiteSpace(_viewModel.NameNumber))
+            {
+                if (!int.TryParse(_viewModel.NameNumber.Trim(), out number))
+                {
+                    //TODO need an error message
+                    return;
+                }
+            }
+
+            var addressResults = await _addressService.FindByPostCodeAsync(_viewModel.PostCode, number == default ? (int?)null : number);
+            _viewModel.Addresses = new List<AddressFindResult.Address>(addressResults.Addresses);
         }
     }
 }
